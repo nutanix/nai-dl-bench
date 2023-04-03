34cdf05e5c54e68a4b153be73be1baffc75c2e52
Gavrish Prabhu <gavrish.prabhu@nutanix.com>
2023-04-03 17:05:07 +0000

    Add appropriate comments

--- inference/code/torchserve/torchserve_run.py
+++ inference/code/torchserve/torchserve_run.py
@@ -128,6 +128,8 @@ def cleanup(gen_folder, ts_stop = True, ts_cleanup = True):
         ts.stop_torchserve()
         dirpath = os.path.dirname(__file__)
         # clean up the logs folder to reset logs before the next run
+        # TODO - To reduce logs from taking a lot of storage it is being cleared everytime it is stopped
+        # Understand on how this can be handled better by rolling file approach
         rm_dir(os.path.join(dirpath, 'utils', gen_folder, 'logs'))
 
         if ts_cleanup:
