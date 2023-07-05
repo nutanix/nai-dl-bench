b54b682e9a45f2ef3ce143b4bd8f2e52d211262f
Gavrish Prabhu <gavrish.prabhu@nutanix.com>
2023-07-05 05:23:14 +0000

    NVD changes for container

---
+++ inference/code/torchserve/kubernetes_run.py
@@ -0,0 +1,88 @@
+from kubernetes import client, config
+import argparse
+import sys
+# Create the argument parser
+parser = argparse.ArgumentParser(description='Script to generate the yaml.')
+
+# Add arguments
+parser.add_argument('--image', type=str, help='image name')
+parser.add_argument('--command', type=str, help='execution command')
+parser.add_argument('--gpu', type=int, help='number of gpus')
+parser.add_argument('--cpu', type=int, help='number of cpus')
+parser.add_argument('--mem', type=str, help='memory required by the container')
+
+kubMemUnits = ['Ei', 'Pi', 'Ti', 'Gi', 'Mi', 'Ki']
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+if not any(unit in args.mem for unit in kubMemUnits):
+    print("container memory unit has to be one of", kubMemUnits)
+    sys.exit(1)
+
+gpus = args.gpu
+cpus = args.cpu
+memory = args.mem
+image = args.image
+command = args.command.split(' ')
+# Access the parsed arguments
+
+config.load_kube_config()
+api = client.AppsV1Api()
+
+deployment = client.V1Deployment(
+    api_version="apps/v1",
+    kind="Deployment",
+    metadata=client.V1ObjectMeta(
+        name="torchserve"
+    ),
+    spec=client.V1DeploymentSpec(
+        replicas=1,
+        selector=client.V1LabelSelector(
+            match_labels={
+                "app": "torchserve"
+            }
+        ),
+        template=client.V1PodTemplateSpec(
+            metadata=client.V1ObjectMeta(
+                labels={
+                    "app": "torchserve"
+                }
+            ),
+            spec=client.V1PodSpec(
+                containers=[
+                    client.V1Container(
+                        name="torchserve",
+                        image=image,
+                        args=command,
+                        image_pull_policy="IfNotPresent",
+                        ports=[
+                            client.V1ContainerPort(
+                                name="ts",
+                                container_port=8080
+                            ),
+                            client.V1ContainerPort(
+                                name="ts-management",
+                                container_port=8081
+                            ),
+                            client.V1ContainerPort(
+                                name="ts-metric",
+                                container_port=8082
+                            )
+                        ],
+                        resources=client.V1ResourceRequirements(
+                            limits={
+                                "cpu": cpus,
+                                "memory": memory,
+                                "nvidia.com/gpu": gpus
+                            }
+                        )
+                    )
+                ]
+            )
+        )
+    )
+)
+
+api.create_namespaced_deployment(namespace='default', body=deployment)
+
--- inference/code/torchserve/utils/system_utils.py
+++ inference/code/torchserve/utils/system_utils.py
@@ -64,3 +64,10 @@ def check_if_path_exists(filepath):
     if(not os.path.exists(filepath)):
         print(f"Filepath does not exist - {filepath}")
         sys.exit(1)
+
+def create_folder_if_not_exits(path):
+    isExist = os.path.exists(path)
+    if not isExist:
+        #  Create a new directory because it does not exist
+        os.makedirs(path)
+        print(f"The new directory is created! - {path}")
